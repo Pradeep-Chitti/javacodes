11e51ca8e01cf3e514c627b71f1987655020152a
Pradeep9036455410 <pradeep.xworkz@gmail.com>
2022-02-27 20:22:18 +0530

    Uploding DataTypes

---
+++ DataTypes/datatype.java
@@ -0,0 +1,23 @@
+class chair
+{
+	public static void main (String[]pradeep)
+	{
+		byte noOfLegs =4;
+		System.out.println(noOfLegs);
+		short VehicalNo=8080;
+		System.out.println(VehicalNo);
+		int Pincode =587203;
+		System.out.println(Pincode);
+		long serNO =1234654555555626l;
+		System.out.println(serNO);
+		float bus=1.50f;
+		System.out.println(bus);
+		double van=1.23456789123;
+		System.out.println(van);
+		char name='P';
+		System.out.println(name);
+		boolean available=true;
+		System.out.println(available);
+	}
+}
+		
