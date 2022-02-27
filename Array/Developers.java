d74a8a23328be8194673c60deb245f2764dfe56f
Pradeep9036455410 <pradeep.xworkz@gmail.com>
2022-02-27 20:12:23 +0530

    uploding Array

---
+++ Array/Developers.java
@@ -0,0 +1,97 @@
+class Developers
+		{
+		public static void main(String []args)
+		{
+		String Names[]={"Shri" ,"Chetu" ,"Ganni" ,"Darsh" ,"Shivuuu" ,"Arpi" ,"Anu" ,"Saaru" ,"Pradee" ,"Niku" ,"Pandu",};
+		
+		System.out.println(Names.length);
+		
+		System.out.println(Names[0]);
+		System.out.println(Names[1]);
+		System.out.println(Names[2]);
+		System.out.println(Names[3]);
+		System.out.println(Names[4]);
+		System.out.println(Names[5]);
+		System.out.println(Names[6]);
+		System.out.println(Names[7]);
+		System.out.println(Names[8]);
+		System.out.println(Names[9]);
+		System.out.println(Names[10]);
+		
+		System.out.println("==================================");
+		
+		// printing all names
+		String a=Names[5];
+		System.out.println(a);
+		
+		Names[5]="Arpita";
+		System.out.println(Names[5]);
+		System.out.println("===================================");
+		
+		
+		String b=Names[0];
+		System.out.println(b);
+		
+		Names[0]="Shrishail";
+		System.out.println(Names[0]);
+		System.out.println("===================================");
+		
+		String c=Names[1];
+		System.out.println(c);
+		
+		Names[1]="Chetan";
+		System.out.println(Names[1]);
+		System.out.println("===================================");
+		
+		String d=Names[2];
+		System.out.println(d);
+		Names[2]="Ganesh";
+		System.out.println(Names[2]);
+		System.out.println("=====================================");
+		
+		String e=Names[3];
+		System.out.println(e);
+		Names[3]="Darshan";
+		System.out.println(Names[3]);
+		System.out.println("========================================");
+		
+		String f=Names[4];
+		System.out.println(f);
+		Names[4]="Shivshankar";
+		System.out.println(Names[4]);
+		System.out.println("========================================");
+		
+		
+		String g=Names[6];
+		System.out.println(g);
+		Names[6]="Anusha";
+		System.out.println(Names[6]);
+		System.out.println("===========================================");
+		
+		
+		String h=Names[7];
+		System.out.println(h);
+		Names[7]="Saru";
+		System.out.println(Names[7]);
+		System.out.println("=============================================");
+	
+		String i=Names[8];
+		System.out.println(i);
+		Names[8]="Pradeep";
+		System.out.println(Names[8]);
+		System.out.println("=============================================");
+		
+		
+		String j=Names[9];
+		System.out.println(j);
+		Names[9]="Nikunj";
+		System.out.println(Names[9]);
+		System.out.println("=============================================");
+	
+	
+		String k=Names[10];
+		System.out.println(k);
+		Names[10]="Pandya420";
+		System.out.println(Names[10]);
+	}
+		}
---
+++ Array/DevelopersEmails.java
@@ -0,0 +1,71 @@
+class DevelopersEmails
+		{
+				public static void main(String []args)
+				{
+				String[]Emails={"pradeep@gmail.com" ,"chetan@gmail.com" ,"ganesh@gmail.com" ,"shrishail@gmail.com" ,"pandya@gmail.com" ,"chandu@gmail.com" , "tej@gmail.com"};
+				{
+				System.out.println(Emails.length);
+				
+				
+				System.out.println(Emails[0]);
+				System.out.println(Emails[1]);
+				System.out.println(Emails[2]);
+				System.out.println(Emails[3]);
+				System.out.println(Emails[4]);
+				System.out.println(Emails[5]);
+				System.out.println(Emails[6]);
+
+				System.out.println("=====================");
+				
+				// Emails Editing
+				String a=Emails[0];
+				System.out.println(a);
+				Emails[0]="pradeep123@gmail.com";
+				System.out.println(Emails[0]);
+				System.out.println("=====================");
+				
+				
+				String b=Emails[1];
+				System.out.println(b);
+				Emails[1]="chetan123@gmail.com";
+				System.out.println(Emails[1]);
+				System.out.println("========================================");	
+
+				String c=Emails[2];
+				System.out.println(c);
+				Emails[2]="ganesh123@gmail.com";
+				System.out.println(Emails[2]);
+				System.out.println("=========================================");
+				
+				
+				
+				String d=Emails[3];
+				System.out.println(d);
+				Emails[3]="shrishail123@gmail";
+				System.out.println(Emails[3]);
+				System.out.println("===========================================");
+				
+				
+				String e=Emails[4];
+				System.out.println(e);
+				Emails[4]="pandyaa420@gmail";
+				System.out.println(Emails[4]);
+				System.out.println("============================================");
+				
+				
+				String f=Emails[5];
+				System.out.println(e);
+				Emails[5]="chandru123@gmail.com";
+				System.out.println(Emails[5]);
+				System.out.println("=============================================");
+				
+				
+				
+				String g=Emails[6];
+				System.out.println(g);
+				Emails[6]="tej123@gmail.com";
+				System.out.println(Emails[6]);
+				}
+				}
+				}
+			
