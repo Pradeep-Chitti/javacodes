3fef3bd194b82ec6b6eb1e1943ad15902cf89e5c
Pradeep9036455410 <pradeep.xworkz@gmail.com>
2022-02-27 20:17:30 +0530

    Uploding Methods

---
+++ Methods/Laptop.java
@@ -0,0 +1,31 @@
+class Laptop
+{
+	public static void main(String []args0)
+		{
+            printModel ();
+		printModel();
+		printModel();
+		printModel("HP");
+		printModel1("Pradeep");
+		
+        }
+		static void printModel()
+		{
+		System.out.println("Easy to carry");
+		System.out.println("Store information");
+		System.out.println("game");
+	
+		}
+		static void printModel(String Brand)
+		{
+		System.out.println(Brand+"Easy to carry");
+	      System.out.println(Brand+"Store information");
+		System.out.println(Brand+"game");
+		}
+		
+		static void printModel1(String Name)
+		{
+		System.out.println(Name+"nav hing");
+		System.out.println(Name+"Hawdu");		
+		}
+		}
---
+++ Methods/Laptop1.java
@@ -0,0 +1,14 @@
+class Laptop1
+{
+		public static void main(String []args)
+		{
+	    float result= Laptop1.getPrice();
+		System.out.println(result);
+		}
+		static float getPrice()
+		{
+		float p=45000.3f;
+		return p;
+		}
+		}
+		
