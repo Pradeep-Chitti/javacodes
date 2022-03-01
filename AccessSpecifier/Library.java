2dfc6ca3f1175cbd1079e19a9ff0610adb1ef3e7
Pradeep9036455410 <pradeep.xworkz@gmail.com>
2022-03-01 10:42:08 +0530

    add folder : added AccessSpecifier

---
+++ AccessSpecifier/Library.java
@@ -0,0 +1,28 @@
+class Library {
+
+	private static void book()
+	{
+		System.out.println("Mahabarat book");
+	}
+	
+	static void bookname()
+	{
+		System.out.println("Mahabarat book");
+	}
+	
+	static void bookPrice(double price)
+	{
+	System.out.println(price);
+	}
+	public static void bookAuthor(String name,String publication)
+	{
+	System.out.println(name + publication);
+	}
+	public static void bookPages(int page,String font)
+	{
+		System.out.println(page + font);
+	}
+	
+	
+	}
+
---
+++ AccessSpecifier/libry.java
@@ -0,0 +1,12 @@
+
+public class libry 
+{
+	public static void main(String []args)
+	{
+		Library.bookname();
+		Library.bookPrice(500.50);
+		Library.bookAuthor("Vyasa","Dev publication");
+		Library.bookPages(500,"Italic");
+	}
+
+}
